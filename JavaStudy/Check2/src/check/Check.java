dd77770b1fb04c2fd737ad6837ae349847faf332
Tomoya <tomoya10632140@gmail.com>
2021-06-25 03:32:19 +0900

    修正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -4,10 +4,10 @@ import constants.Constants;
 
 public class Check {
 	
-	private String firstName = "樋口";
-	private String lastName = "知也";
+	private static String firstName = "樋口";
+	private static String lastName = "知也";
 	
-	private void printName(String firstName,String lastName){
+	private static void printName(String firstName,String lastName){
 		System.out.println("printNameメソッド → " + firstName + lastName);
 	}
 
@@ -15,12 +15,10 @@ public class Check {
 
 	public static void main(String[] args) { 
 	
-		Constants con = new Constants();
 		Pet pet = new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
 		RobotPet robot = new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
-		Check ch = new Check();
 		
-		ch.printName(ch.firstName,ch.lastName);
+		printName(firstName,lastName);
 		pet.introduce();
 		robot.introduce();
 		
